23fb17195dfe933d7ea6dca4351ac8d2c0e278bd
Myo Aung <myoaungg213@gmail.com>
2025-07-22 21:18:11 +0700

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css'
 import Dashboard from './components/Dashboard';
 import BookDetail from './components/BookDetail';
 import Sidebar from './components/Sidebar';
+import './App.css'; // make sure this includes .app-layout styles
 
 const App = () => {
   return (
     <div className="app-layout">
       <Sidebar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/book/:bookKey" element={<BookDetail />} />
-      </Routes>
+      <div className="routes-content">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/book/:bookKey" element={<BookDetail />} />
+        </Routes>
+      </div>
     </div>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import '../Sidebar.css'
 
 const Sidebar = () => (
   <div className="sidebar">
